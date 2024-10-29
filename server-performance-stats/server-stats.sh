fd22ead80356de64ce25e0d287106203ff5d947d
Dawid Kuzbiel <73785011+madebydawid@users.noreply.github.com>
2024-10-29 15:47:18 +0100

    Update server-stats.sh
    Updated script to include colors and headers for easy readability.

--- server-performance-stats/server-stats.sh
+++ server-performance-stats/server-stats.sh
@@ -2,38 +2,53 @@
 
 # get total CPU usage
 get_cpu_usage() {
-    top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}'
+    print_colored "CPU Usage:"
+    local cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')
+    echo $cpu_usage
 }
 
 # get total memory usage
 get_memory_usage() {
-    free | grep Mem | awk '{print "Used: " $3/$2 * 100.0 "%"}'
+    print_colored "Memory Usage:"
+    local mem_usage=$(free | grep Mem | awk '{print "Used: " $3/$2 * 100.0 "%"}')
+    echo $mem_usage
 }
 
 # get total disk usage
 get_disk_usage() {
-    df --output=source,size,used,avail,pcent / | tail -1
+    print_colored "Disk Usage:"
+    local disk_usage=$(df --output=source,size,used,avail,pcent / | tail -1)
+    echo $disk_usage
 }
 
 # get top 5 processes by CPU usage
 get_top_cpu_processes() {
+    print_colored "Top 5 CPU Processes:"
     ps -eo pid,comm,%cpu --sort=-%cpu | head -n 6
 }
 
 # get top 5 processes by memory usage
 get_top_memory_usage() {
+    print_colored "Top 5 Memory Processes:"
     ps -eo pid,comm,%mem --sort=-%mem | head -n 6
 }
 
 # Additional stats
 get_additional_stats() {
-    echo "OS Version: $(uname -a)"
-    echo "Uptime: $(uptime -p)"
-    echo "Logged in users: $(who | wc -l)"
+    print_colored "Additional Stats:"
+    echo -e "\033[1;37mOS Version: $(uname -a)\033[0m"
+    echo -e "\033[1;37mUptime: $(uptime -p)\033[0m"
+    print_colored "Logged in users: \033[1;37m$(who | wc -l)\033[0m"
+    print_colored "Current user: \033[1;37mdawid\033[0m"
+}
+
+# Färglägga text i cyan
+print_colored() {
+    echo -e "\033[1;36m$1\033[0m"
 }
 
 main() {
-    echo "Server Performance Stats"
+    echo -e "\033[1;36mServer Performance Stats\033[0m"
     echo "========================"
     
     get_cpu_usage
